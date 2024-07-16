0eb027f997374e6e1eee385923f1604dbce9f994
Ahmad-s2004 <shahidahmadamman@gmail.com>
2024-07-16 17:24:46 +0500

    commit

--- middleware/isLogIn.js
+++ middleware/isLogIn.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const key = 'ahmad2006';
 
 const isLoggedIn = (req, res, next) => {
-    const token = req.cookies.token; 
+    const token = req.headers.authorization
 
     if (!token) {
         return res.status(404).json({ message: "Token not Found", token });
