6cf6a3c6e669e4d2e8b836835040b5149fa95c64
Ahmad-s2004 <shahidahmadamman@gmail.com>
2024-07-16 14:37:28 +0500

    commit

--- controllers/user.js
+++ controllers/user.js
@@ -103,7 +103,7 @@ const getAddress = async (req, res) => {
         return res.status(201).json({ message: "Address added successfully", data: findData });
     } catch (error) {
         console.error("Error in getAddress handler:", error);
-        return res.status(500).json({ message: "Internal server error" });
+        return res.status(500).json({ message: "Internal server error",id});
     }
 };
 
