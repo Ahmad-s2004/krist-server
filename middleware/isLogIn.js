85271e7348f3eb7f7009e1e319f52e3492da28fa
Ahmad-s2004 <shahidahmadamman@gmail.com>
2024-08-01 22:18:16 +0500

    commit

--- middleware/isLogIn.js
+++ middleware/isLogIn.js
@@ -2,23 +2,19 @@ const jwt = require('jsonwebtoken');
 const key = 'ahmad2006';
 
 const isLoggedIn = (req, res, next) => {
-    // const token = req.headers.authorization
-    const token = req.cookies
-
+    const token = req.headers.authorization
 
     if (!token) {
         return res.status(404).json({ message: "Token not Found", token });
     }
-    let cookie = JSON.parse(token)
+
     try {
-        const decoded = jwt.verify(cookie, key);
+        const decoded = jwt.verify(token, key);
         req.user = decoded;
         next();
     } catch (err) {
         console.error("Error in isLoggedIn middleware:", err);
-        let type = typeof(token)
-        let newtype = typeof(cookie)
-        return res.status(401).json({ message: "Invalid token" , err, token, type, cookie, newtype});
+        return res.status(401).json({ message: "Invalid token" });
     }
 };
 
